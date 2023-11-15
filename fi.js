5192afb2a04308d8d8573520c4f3caa67268925d
lendersiq <lendersiq@gmail.com>
2023-11-14 19:51:49 -0500

    Update fi.js
    debug 2

--- fi.js
+++ fi.js
@@ -48,7 +48,7 @@ function $$calculate(str) {
 function $$process_dicts(arg) {
     tag_strip = arg.replace(/<\/?[^>]+(>|$)/g, '')
     _args = tag_strip.replace(/[\[\]']+/g,'').split(':')
-    console.log('dicts', _args)
+    console.log('dicts', args)
     if ( _args[1] == 'undefined' ) {
         return 0
     } else {
